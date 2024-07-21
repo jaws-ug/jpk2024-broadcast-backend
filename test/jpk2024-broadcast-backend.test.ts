a38c66afe5766c74c0ebe749008ed7e5a2d135ab
soda1p2 <40328218+soda1p2@users.noreply.github.com>
2024-07-21 07:35:53 +0000

    #1 initialize CDK Project

---
+++ test/jpk2024-broadcast-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Jpk2024BroadcastBackend from '../lib/jpk2024-broadcast-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/jpk2024-broadcast-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Jpk2024BroadcastBackend.Jpk2024BroadcastBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
