cdb13064155f8f3772f22b9585ece18bfdfa147f
soda1p2 <40328218+soda1p2@users.noreply.github.com>
2024-08-17 08:00:00 +0000

    #2 Add Multilanguage Translation

--- src/translate-function.handler.ts
+++ src/translate-function.handler.ts
@@ -1,5 +1,6 @@
 import { Translate } from 'aws-sdk'
 import { APIGatewayProxyEvent } from 'aws-lambda'
+import { Logging } from 'aws-cdk-lib/custom-resources';
 
 const translate = new Translate()
 
@@ -16,16 +17,59 @@ export const handler = async (event: APIGatewayProxyEvent) => {
     }
     const Text = requestBody.text
     const SourceLanguageCode = requestBody.translateFrom
-    const TargetLanguageCode = requestBody.translateTo
+    //const TargetLanguageCode = requestBody.translateTo
+    /**
+     * Supported languages and language codes - Amazon Translate
+     * https://docs.aws.amazon.com/translate/latest/dg/what-is-languages.html
+     */
+    const TargetLanguageCodes = [
+      'en', //English
+      'zh-TW', //Chinese (Traditional)
+      'zh', //Chinese (Simplified)
+      'hi', //Hindi
+      'es', //Spanish
+      'es-MX', //Spanish (Mexico)
+      'fr', //French
+      'fr-CA', //French (Canada)
+      'id', //Indonesian
+      'pt-PT', //Portuguese (Portugal)
+      'pt', //Portuguese (Brazil)
+      'ru', //Russian
+      'ur', //Urdu
+      'tl', //Filipino, Tagalog
+      'ja', //Japanese
+      'de', //German
+      'ha', //Hausa
+      'bn', //Bengali
+      'vi', //Vietnamese
+      'cy', //Welsh
+      'ko', //Korean
+      'it', //Italian
+      'am', //Amharic
+      'ar' //Arabic
+    ]
+    /// delete Source Language code from array
+    delete TargetLanguageCodes[TargetLanguageCodes.findIndex(item => item === SourceLanguageCode)]
 
-    const translateParams = {
-      Text,
-      SourceLanguageCode,
-      TargetLanguageCode,
+    let responceBody:any = {
+      "originalText": requestBody.text,
+      "SourceLanguageCode": requestBody.translateFrom,
+    };
+    for (const TargetLanguageCode of TargetLanguageCodes){
+      const translateParams = {
+        Text,
+        SourceLanguageCode,
+        TargetLanguageCode,
+      }
+      console.log("Text:" + translateParams.Text)
+      console.log("SourceLanguageCode:" + translateParams.SourceLanguageCode)
+      console.log("TargetLanguageCode:" + translateParams.TargetLanguageCode)
+      // translatedText
+      const translatedText = await translate.translateText(translateParams).promise()
+      // Add Translate results to Array
+      responceBody.TargetLanguageCode = translatedText
+      console.log("translatedText:" + responceBody.TargetLanguageCode)
     }
-
-    const translatedText = await translate.translateText(translateParams).promise()
-
     return {
       statusCode: 200,
       headers: {
@@ -33,7 +77,7 @@ export const handler = async (event: APIGatewayProxyEvent) => {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'POST',
       },
-      body: JSON.stringify({ translatedText }),
+      body: JSON.stringify({ responceBody }),
     }
   } catch (error) {
     return {
