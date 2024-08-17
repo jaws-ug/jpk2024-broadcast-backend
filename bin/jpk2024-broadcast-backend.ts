2ebe308dc3d211b1bb9a3700cab9c284a471b534
hidetoshi-matsui <hidetoshi.matsui@dotdata.com>
2024-08-17 12:13:51 +0900

    [Add]ivs viewers count stack to main stack

--- bin/jpk2024-broadcast-backend.ts
+++ bin/jpk2024-broadcast-backend.ts
@@ -3,7 +3,9 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { Jpk2024BroadcastBackendTranslateFunctionStack } from '../lib/jpk2024-broadcast-backend-translate-function-stack';
 import { Jpk2024BroadcastBackendDbStack } from '../lib/jpk2024-broadcast-backend-db-stack';
+import { Jpk2024BroadcastBackendIvsViewersCountStack } from '../lib/jpk2024-broadcast-backend-ivs-viewers-count-stack';
 
 const app = new cdk.App();
 new Jpk2024BroadcastBackendDbStack(app, 'Jpk2024BroadcastBackendDbStack');
 new Jpk2024BroadcastBackendTranslateFunctionStack(app, 'Jpk2024BroadcastBackendTranslateFunctionStack');
+new Jpk2024BroadcastBackendIvsViewersCountStack(app, 'Jpk2024BroadcastBackendIvsViewersCountStack');
