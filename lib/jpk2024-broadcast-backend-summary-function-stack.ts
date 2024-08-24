43da5861f146877d543135eee2ad56fa54b0a5f3
soda1p2 <40328218+soda1p2@users.noreply.github.com>
2024-08-24 09:50:10 +0000

    Add Bedrock Summary API

--- lib/jpk2024-broadcast-backend-summary-function-stack.ts
+++ lib/jpk2024-broadcast-backend-summary-function-stack.ts
@@ -3,12 +3,14 @@ import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
 import { ITableV2, TableV2 } from 'aws-cdk-lib/aws-dynamodb';
 import { Runtime } from 'aws-cdk-lib/aws-lambda'
 import { Construct } from 'constructs'
-
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam'
 
 export class Jpk2024BroadcastBackendSummaryFunctionStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const translateResultTableArn = Fn.importValue('translateResultTableArn');
+    const translateResultTable: ITableV2 = TableV2.fromTableArn(this, 'translateResult', translateResultTableArn);
     const summaryResultTableArn = Fn.importValue('summaryResultTableArn');
     const summaryResultTable: ITableV2 = TableV2.fromTableArn(this, 'summaryResultTable', summaryResultTableArn);
 
@@ -24,10 +26,19 @@ export class Jpk2024BroadcastBackendSummaryFunctionStack extends Stack {
         RESULT_TABLE_SORT_KEY: 'date',
         SUMMARY_TABLE_NAME: summaryResultTable.tableName,
         SUMMARY_TABLE_PRIMARY_KEY: 'sessionId',
-        SESSIONID: ''
+        SEARCHWORD: 'F300'
       },
     })
-
+    // add readWrite permission to the translateResultTable
+    translateResultTable.grantReadData(summaryFunction);
     summaryResultTable.grantReadWriteData(summaryFunction);
+
+    summaryFunction.addToRolePolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: ["bedrock:InvokeModel", "bedrock:InvokeModelWithResponseStream"],
+        resources: ["*"],
+      })
+    )
   }
 }
